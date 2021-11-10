5baf22737c6f2b34b556248c182c208989dc4191
Alin Salaru <mplacintasalaru@deloittece.com>
2021-11-10 13:10:43 +0200

    basic react router app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes, Link, useParams, useLocation, useSearchParams } from "react-router-dom";
+
+const Hello = () => (<h2>Hello world</h2>)
+
+const Bye = () => (<div><h2>Bye!</h2> <Link to="/"> ET phoned home</Link> </div>)
+
+const WhereAmI  = () => {
+  const { place } = useParams();
+  const location = useLocation();
+  if(place) {
+    return (<div>
+      I am on: {place}
+      </div>)
+  }
+  return <div>nowhere</div>
+ 
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hello />}></Route>
+          <Route path="/bye" element={<Bye />}></Route>
+          <Route path="/whereami" element={<WhereAmI />}>
+          <Route
+                index
+                element={
+                  <main style={{ padding: "1rem" }}>
+                    <p>Select an invoice</p>
+                  </main>
+                }
+              />
+            <Route path=":place" element={<WhereAmI />} />
+          </Route>
+          <Route path="*" element={<div> 404 not found </div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
